93950f99d16bea45fb58e35a7349d9ddb6acbf4e
natalieh93 <natalieh93@users.github.com>
2021-06-15 17:51:55 +0200

    Added links to LinkedIn and GitHub

--- src/App.js
+++ src/App.js
@@ -12,7 +12,28 @@ export default function App() {
         <Dictionary />
       </main>
       <footer className="App-footer">
-       Coded by Natalie Hood</footer>
+        <span>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/natalie-hood-4b088a10b/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="linkedin-link"
+          >
+            <strong> Natalie Hood</strong>
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/natalieh93/shecodes-react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="github-link"
+          >
+            <strong>open-sourced</strong>
+          </a>{" "}
+          on GitHub.
+        </span>
+      </footer>
     </div>
   );
 }
