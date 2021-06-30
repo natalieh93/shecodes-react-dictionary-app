2bc2c6d09a8ff5d1dbce2bdb2d7ad0d3bc883b14
natalieh93 <natalieh93@users.github.com>
2021-06-30 18:30:35 +0200

    Added Phonetic component

---
+++ src/Phonetic.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export default function Phonetic (props) {
+    const audio = new Audio (props.phonetic.audio)
+    function playSound() {
+        audio.play();
+    }
+    return (
+        <div className="Phonetic">
+            <span className="sound">
+           <i className="fas fa-volume-up" onClick={playSound}></i>
+           </span>
+           <span className="text">{""} [ {props.phonetic.text} ]</span> 
+        </div>
+    );
+}
